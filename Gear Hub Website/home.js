6b44a105604f39f3d63091d699681801cd0f64af
Abdalrahman01-CS <abdalrahman2018home@gmail.com>
2025-04-05 21:22:16 +0200

    Organize files into subfolders

--- Gear Hub Website/home.js
+++
@@ -1,8 +0,0 @@
-// This function for nav of home page
-
-const menu = document.querySelector("#menu");
-const links = document.querySelector(".homeLinks");
-
-menu.onclick = function showMenu() {
-    links.classList.toggle("active");
-};
